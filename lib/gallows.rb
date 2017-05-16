b792006e888c9b84f7537b01924a5c2a1d45f452
jfonz412 <jfonzone@outlook.com>
2017-05-16 19:09:48 -0400

    put public in it's proper spot

--- lib/gallows.rb
+++ lib/gallows.rb
@@ -4,13 +4,13 @@ class Gallows
   def initialize
   	@turns = 0
     @man =  [ 
-    	      "public/images/hangman.jpg",
+    	      "/images/hangman.jpg",
     	      "/images/hangman_1.jpg",
     	      "/images/hangman_2.jpg",
-    	      "images/hangman_3.jpg",
-    	      "images/hangman_4.jpg",
-    	      "images/hangman_5.jpg",
-    	      "images/hangman_6.jpg"
+    	      "/images/hangman_3.jpg",
+    	      "/images/hangman_4.jpg",
+    	      "/images/hangman_5.jpg",
+    	      "/images/hangman_6.jpg"
     	    ]  
     @image = @man[@turns]          
   end
@@ -23,3 +23,4 @@ class Gallows
   	@image = @man[@turns]
   end
 end
+
